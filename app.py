04390eb06626daf267de9f3c3afb8425d74c16c9
Lucas :D <frade052@gmail.com>
2024-01-17 02:16:10 -0300

    Nova versão do sistema. Muita coisa nova (sei que não é uma boa mensagem de commit)

--- app.py
+++ app.py
@@ -2,15 +2,9 @@ import os
 
 
 restaurantes = []
-# Para mais tarde
-# try:
-#     restaurantes_ativos = {
-#         restaurantes[0]: {
-#             'ativo': 1
-#         }
-#     }
-# except IndexError:
-#     pass
+restaurantes_ativos = {}
+
+print(restaurantes_ativos)
 
 def nome_do_programa():
     """
@@ -25,9 +19,20 @@ def opcoes():
     print("""
     1. Cadastrar o restaurante
     2. Listar restaurantes
-    3. Ativar novamente
-    4. Fechar
+    3. Ativar restaurante
+    4. Mais
     """)
+
+def opcoes2():
+    """
+    Mostra o restante das opções
+    """
+    print("""
+    5. Excluir restaurante
+    6. Desativar restaurante
+    7. Listar restaurantes ativados e número de ativos
+    8. Fechar
+""")
 
 def finalizar_app():
     """
@@ -40,20 +45,20 @@ def finalizar_app():
     print('Fechando o app\n')
     print()
 
-def opcao_invalida():
+def mensagem_de_erro(erro):
     """
     Envia a mensagem de opção enviada ao console
 
     """
-    exibi_subtitulo('Erro! Opção Ínvalida')
-    input('Digite alguma tecla para retornar: ')
-    main()
+    erros = ['Opção Inválida.', 'Nome de restaurante inválido.', 'Nome do restaurante já existe.', 'Esse restaurante não existe.', 'Ainda não existem restaurantes','Nenhum restaurante foi ativado.']
+    exibi_msg_erro(f'Erro! {erros[erro]}')
+    volta_menu()
 
 def decisao():
     """
     Aplica a escolha do usuário com base no que foi inserido
     Retorna:
-    function: retorna as funções cadastro_novo_restaurante, listar_restaurante, finalizar_app e main
+    function: retorna as funções cadastro_novo_restaurante, listar_restaurante, opcoes2, mensagem_de_erro e main
     """
     try:
         opcao_do_usuario = int(input('Digite a sua escolha: '))
@@ -65,23 +70,71 @@ def decisao():
             case 3:
                 ativa_restaurante()
             case 4:
+                decisao2()
+            case _:
+                mensagem_de_erro(0)
+    except ValueError:
+        mensagem_de_erro(0)
+        volta_menu()
+
+def decisao2():
+    """
+    Aplica a escolha do usuário da página dois das opções
+    Retorna: 
+    function: executa as funções exclui_restaurante, desativa_restaurante() e finalizar_app()
+    """
+    try:
+        exibi_subtitulo2('SmallCON  Segunda página de opções')
+        opcoes2()
+        opcao_do_usuario = int(input('Digite a opção: '))
+        match opcao_do_usuario:
+            case 5:
+                exclui_restaurante()
+            case 6:
+                desativa_restaurante()
+            case 7:
+                lista_ativos()
+                # imprime_dict()
+            case 8:
                 finalizar_app()
             case _:
-                opcao_invalida()
+                mensagem_de_erro(0)
     except ValueError:
-        opcao_invalida()
-        main()
+        mensagem_de_erro(0)
 
 def exibi_subtitulo(txt):
     """
     Imprime o subtítulo da opção
     Limpa o terminal
+    Aplica um espaço depois     da mensagem
     Args:
-        txt (_type_): _description_
+        txt (_type_): _mensagem do subtítulo_
+    """
+    os.system('cls')
+    print(f'SmallCON - {txt}')
+    print()
+
+def exibi_msg_erro(txt):
+    """
+    Template de mensagem de erro aonde não aparece não aparece o nome do sistema
+
+    Args:
+        txt (_str_): _mensagem do erro_
     """
     os.system('cls')
     print(txt)
     print()
+
+
+def exibi_subtitulo2(txt):
+    """
+    Imprime o subtítulo da opção
+    Limpa o terminal
+    Args:
+        txt (_type_): _mensagem do subtítulo_
+    """
+    os.system('cls')
+    print(f'SmallCON - {txt}')
 
 def main():
     """
@@ -100,9 +153,27 @@ def cadastrar_novo_restaurante():
     """
     exibi_subtitulo('Cadastro de restaurante')
     nome_restaurante = input("Digite o nome do seu restaunte: ")
-    restaurantes.append(nome_restaurante)
-    print(f'O restaurante {nome_restaurante} foi criado')
-    volta_menu()
+    if nome_restaurante != '':
+        if nome_restaurante not in restaurantes:
+            restaurantes.append(nome_restaurante)
+            # Adiciona o restaurante para o dicionário dos ativos mas com valor de 0 (não)
+            adiciona_ativo_0(nome_restaurante)
+            # ----------------------------------
+            print(f'O restaurante {nome_restaurante} foi criado')
+            volta_menu()
+        else:
+            mensagem_de_erro(2)
+    else:
+        print(nome_restaurante)
+        mensagem_de_erro(1)
+
+def adiciona_ativo_0(nome):
+    """
+    Adiciona o restaurante cadastrado com valor de 0
+    Args:
+        nome (_str_): _nome do restaurante para ser pegado o index dele_
+    """
+    restaurantes_ativos.update({restaurantes[acha_index_restaurante(nome)]: {'ativo': 0}})
 
 def listar_restaurante():
     """
@@ -125,13 +196,86 @@ def ativa_restaurante():
     Retorno:
     array: coloca em restaurantes_ativos { restaurante[] }
     """
+    exibi_subtitulo('Ativando restaurante')
     if existe_restaurantes():
         exibi_subtitulo('Ativando o restaurante')
         nome_do_restaurante = verifica_restaurante()
         index_restaurante = acha_index_restaurante(nome_do_restaurante)
-    else:
-        print('Ainda não foi cadastrado nenhum restaurante.')
+        restaurantes_ativos.update({restaurantes[index_restaurante]: {'ativo': 1}})
+        print('Restaurante ativado!')
         volta_menu()
+    else:
+        mensagem_de_erro(4)
+        volta_menu()
+
+# Opcões 2 -------------------------------------------
+
+def exclui_restaurante():
+    """
+    Exclui um restaurante existente
+    Retorna:
+    list: a lista atualizada com os restaurantes existentes
+    """
+    if existe_restaurantes():
+        exibi_subtitulo('Excluindo restaurante')
+        nome_restaurante = input('Digite o nome do restaurante: ')
+        if esse_restaurante_existe(nome_restaurante):
+            index = acha_index_restaurante(nome_restaurante)
+            restaurantes_ativos.pop(restaurantes[index])
+            restaurantes.pop(index)
+            print(f'O restaurante {nome_restaurante} foi excluído!')
+            volta_menu()
+    else:
+        mensagem_de_erro(4)
+
+def desativa_restaurante():
+    """
+    Desativa um restaurante ativo
+    Retorna:
+    dict: dicionário com os restaurantes ativos atualizados
+    """
+    if existe_restaurantes():
+        exibi_subtitulo('Desativa restaurante')
+        nome_restaurante = input('Digite o nome do restaurante que vai ser desativado: ')
+        if esse_restaurante_existe(nome_restaurante):
+            index = acha_index_restaurante(nome_restaurante)
+            restaurantes_ativos.update({restaurantes[index]: {'ativo': 0}})
+            print(f'O {nome_restaurante} foi desativado!')
+            volta_menu()
+    else:
+        mensagem_de_erro(4)
+
+def lista_ativos():
+    """
+    Função que lista os restaurantes ativados
+    Soma o número de restaurantes ativados
+    Retorna:
+    str: mensagem com os restaurantes ativos junto do número de restaurantes
+    """
+    if existe_restaurantes():
+        exibi_subtitulo2('Lista/Número de ativo')
+        conta = 0
+        conta1 = 0
+        for index, chave in restaurantes_ativos.items():
+            if chave.get('ativo') == 1:
+                print('Ativos ------')
+                conta += 1
+                print(f'{conta}. {index}')
+            if chave.get('ativo') == 0:
+                print('Inativos ------')
+                conta1 += 1
+                print(f'{conta1}. {index}')
+        print(f'Número de restaurantes ativos: {conta}')
+        print(f'Número de restaurantes inativos: {conta1}')
+        volta_menu()
+    else:
+        mensagem_de_erro(4)
+
+def imprime_dict():
+    """
+    Imprime o dicionário se for preciso. Normalmente usado para debug
+    """
+    print(restaurantes_ativos.items())
 
 def verifica_restaurante():
     """
@@ -141,8 +285,9 @@ def verifica_restaurante():
     if nome_restaurante in restaurantes:
         return nome_restaurante
     else:
-        print('Restaurante não encontrado')
+        mensagem_de_erro(5)
         volta_menu()
+
 
 def acha_index_restaurante(arg):
     """
@@ -174,8 +319,26 @@ def volta_menu():
     Retorna:
     function: executa a função main() que retorna ao menu e executa outras funções como opcoes()
     """
+    print()
     input('Digite algo para voltar ao menu: ')
     main()
+
+def esse_restaurante_existe(nome):
+    """
+    Verifica se o restaurante realmente existe
+    Retorna o erro se não existir
+    Args:
+        nome (_str_): _nome do restaurante_
+    """
+    def cancela_mensagem_de_erro(arg):
+        if arg is True:
+            pass
+        else:
+            mensagem_de_erro(3)
+
+    aqui = nome in restaurantes
+    cancela_mensagem_de_erro(aqui)
+    return aqui
 
 if __name__ == '__main__':
     main()
